a188354bdc7612178bd72cbe429374dbf1b7a712
wiktorlemanski <wiktorlem1@wp.pl>
2025-01-12 02:01:03 +0100

    trasy dla reviews wykorzystuja middleware sprawdzajacy id w params

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -4,14 +4,15 @@ import {getReviewById} from "../controllers/reviewController.js";
 import {editReview} from "../controllers/reviewController.js";
 import {addReview} from "../controllers/reviewController.js";
 import {deleteReview} from "../controllers/reviewController.js"
+import {validateId} from "../middlewares/validateIdMiddleware.js";
 export const reviewRouter = express.Router();
 
 reviewRouter.get('/', getAllReviews )
 
-reviewRouter.get('/:id', getReviewById)
+reviewRouter.get('/:id', validateId('id'),  getReviewById)
 
-reviewRouter.put('/:id/edit', editReview)
+reviewRouter.put('/:id/edit', validateId('id'), editReview)
 
 reviewRouter.post('/', addReview)
 
-reviewRouter.delete('/:id', deleteReview)
+reviewRouter.delete('/:id', validateId('id'), deleteReview)
